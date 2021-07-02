557cb77d5c9dd0fb819b133f4be0b953eac74560
sungminna <sungmin.na330@gmail.com>
2021-07-02 10:46:58 +0900

    6-2
    updown err ??

---
+++ sources/Chapter6/grade.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+array = []
+
+for i in range(n):
+    input_data = input().split()
+
+    array.append((input_data[0], int(input_data[1])))
+
+
+array = sorted(array, key=lambda student: student[1])
+
+for i in array:
+    print(i[0], end=' ')
